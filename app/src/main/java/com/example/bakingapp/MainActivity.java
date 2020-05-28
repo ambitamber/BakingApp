fc305746e6c1033e083e09c6dacd8f8dcf8c9979
Amajit Singh <ambi92@outlook.com>
2020-05-27 21:20:43 -0400

    Added NetworkUtils

--- app/src/main/java/com/example/bakingapp/MainActivity.java
+++ app/src/main/java/com/example/bakingapp/MainActivity.java
@@ -9,15 +9,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.bakingapp.adapter.RecipeAdapter;
 import com.example.bakingapp.model.Recipe;
+import com.example.bakingapp.utils.JsonUtils;
+import com.example.bakingapp.utils.NetworkUtils;
 
+import org.json.JSONException;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements RecipeAdapter.RecipeAdapterOnClickHandler {
 
+    String siteUrl = "https://d17h27t6h515a5.cloudfront.net";
     private RecipeAdapter mAdapter;
     private RecyclerView recyclerView;
     private TextView errorTV;
@@ -41,12 +48,15 @@ public class MainActivity extends AppCompatActivity implements RecipeAdapter.Rec
         recyclerView.setAdapter(mAdapter);
         recyclerView.setHasFixedSize(true);
 
+        URL url = NetworkUtils.builtUrl(siteUrl);
+        new FetchDataTask().execute(url);
     }
 
     @Override
     public void onClick(Recipe recipe) {
         String title = recipe.getTitle();
         String servings = recipe.getServings();
+        Toast.makeText(MainActivity.this,title +" - "+servings,Toast.LENGTH_SHORT).show();
     }
 
     private void showError(){
@@ -65,17 +75,40 @@ public class MainActivity extends AppCompatActivity implements RecipeAdapter.Rec
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            progressBar.setVisibility(View.INVISIBLE);
+            progressBar.setVisibility(View.VISIBLE);
         }
 
         @Override
         protected String doInBackground(URL... urls) {
-            return null;
+
+            if (urls.length == 0){
+                return null;
+            }
+            URL query = urls[0];
+            String searchResults  = null;
+            try {
+                searchResults = NetworkUtils.getResponseFromHttpUrl(query);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return searchResults;
         }
 
         @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
+        protected void onPostExecute(String recipes) {
+            progressBar.setVisibility(View.VISIBLE);
+            if (recipes != null && !recipes.equals("")){
+                showData();
+                try {
+                    mRecipeData = (ArrayList<Recipe>) JsonUtils.getRecipeFromJson(recipes);
+                    mAdapter.setRecipeList(mRecipeData);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }else {
+                showError();
+            }
+
         }
     }
 }
--- app/src/main/java/com/example/bakingapp/utils/JsonUtils.java
+++ app/src/main/java/com/example/bakingapp/utils/JsonUtils.java
@@ -1,6 +1,7 @@
 package com.example.bakingapp.utils;
 
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.example.bakingapp.model.Recipe;
 
@@ -13,6 +14,8 @@ import java.util.List;
 
 public class JsonUtils {
 
+    private static String TAG = JsonUtils.class.getSimpleName();
+
     public static List<Recipe> getRecipeFromJson(String JsonStr) throws JSONException {
         if (TextUtils.isEmpty(JsonStr)){
             return null;
@@ -23,7 +26,8 @@ public class JsonUtils {
         JSONArray jsonArray = new JSONArray(JsonStr);
         for (int i = 0; i < jsonArray.length(); i++){
             JSONObject jsonObject = jsonArray.getJSONObject(i);
-            String title = jsonObject.optString("title");
+            String title = jsonObject.optString("name");
+            Log.i(TAG,"Name: "+ title);
             String servings = jsonObject.optString("servings");
             recipe = new Recipe(title,servings);
             recipeData.add(recipe);
