c392ab73446142ad069687411b6de8ce6785c384
Ambi (Amarjit) Singh <50850921+ambitamber@users.noreply.github.com>
2020-06-02 18:31:46 -0400

    create step fragment_step.xml

--- app/src/main/java/com/example/bakingapp/fragment/IngredientFragment.java
+++ app/src/main/java/com/example/bakingapp/fragment/IngredientFragment.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.bakingapp.R;
@@ -42,19 +44,22 @@ public class IngredientFragment extends Fragment implements IngredientAdapter.In
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment_ingredient, container, false);
         ButterKnife.bind(this, view);
 
         ingredientsList = new ArrayList<>();
         ingredientsList = recipe.getIngredients();
         ingredientAdapter = new IngredientAdapter(ingredientsList, IngredientFragment.this);
-        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 1));
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(ingredientAdapter);
         return view;
     }
 
     @Override
     public void onClick(int index) {
-
+        Ingredients ingredients = new Ingredients();
+        String quantity = ingredients.getQuantity();
+        Toast.makeText(getContext(),"For "+ quantity +"People",Toast.LENGTH_SHORT).show();
     }
 }
